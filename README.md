aa3e98e88bc4a73bd3c11086a42602ac3e17c4da
fde-capu <flacarrara@gmail.com>
2023-05-13 19:24:36 -0300

    First commit.

---
+++ README.md
@@ -0,0 +1,41 @@
+# YouTube Subtitle Downloader
+
+This program downloads subtitles from YouTube videos using the `youtube_transcript_api` and `pytube` packages.
+
+## Installation
+
+1. Install the required packages using the following command:
+
+pip install youtube_transcript_api pytube
+
+## Usage
+
+Run the script with the following command:
+
+python yt-sub.py [<video_id>] [<language_code>]
+
+Replace `<video_id>` with the YouTube video ID and `<language_code>` with the desired subtitle language code (optional).
+
+If you omit the `<video_id>` argument, the script will prompt you for it.
+
+If you omit the `<language_code>` argument, the script will list the available subtitle languages and prompt you to choose one.
+
+## Example
+
+python yt-sub.py PELnMfcYUj0 en
+
+This will download the English subtitles for the video with ID `PELnMfcYUj0`.
+
+## Warnings
+
+This code uses an undocumented part of the YouTube API, which is called by the YouTube web-client. So there is no guarantee that it won't stop working tomorrow, if they change how things work. I will however do my best to make things working again as soon as possible if that happens. So if it stops working, let me know!
+
+## Author
+
+This program was created by Flávio Carrara (fde-capu) flavio@carrara.us.
+
+## Copyright and License
+
+Copyright (c) 2023 Your Name. All rights reserved.
+
+This software is licensed under a custom license that allows for free use, modification, and distribution for non-commercial purposes. For commercial use, a separate license must be obtained. Contact the author for more information on obtaining a commercial license.
---
+++ yt-sub.py
@@ -0,0 +1,52 @@
+"""
+╔═════════════════════════════╗
+║ YouTube Subtitle Downloader ║
+║ Author: fde-capu            ║
+║ Email: flavio@carrara.us    ║
+║ Date: 5/13/2023             ║
+╚═════════════════════════════╝
+"""
+import sys
+from youtube_transcript_api import YouTubeTranscriptApi
+from pytube import YouTube
+
+# Prompt the user for the video id or use the first argument
+if len(sys.argv) > 1:
+    video_id = sys.argv[1]
+else:
+    video_id = input("Enter the video id: ")
+
+# Fetch the video title and author information
+video_url = f"https://www.youtube.com/watch?v={video_id}"
+yt = YouTube(video_url)
+video_title = yt.title
+author_name = yt.author
+
+# Display the author name and video title
+print(f"{author_name}\n{video_title}")
+
+# Retrieve the available transcripts
+transcript_list = YouTubeTranscriptApi.list_transcripts(video_id)
+
+# List available subtitle languages
+transcripts = list(transcript_list)
+for i, transcript in enumerate(transcripts):
+    print(f"{i + 1}. {transcript.language} ({transcript.language_code})")
+
+# Prompt the user to choose a language or use the second argument
+if len(sys.argv) > 2:
+    chosen_language = sys.argv[2]
+else:
+    user_input = int(input("-> "))
+    chosen_language = transcripts[user_input - 1].language_code
+
+# Download the subtitle for the chosen language
+subtitle = YouTubeTranscriptApi.get_transcript(video_id, languages=[chosen_language])
+
+# Save the subtitle to a file
+filename = f"{author_name.replace(' ', '_')}_{video_title[:20].replace(' ', '_')}_{chosen_language}.txt"
+with open(filename, "w") as f:
+    for line in subtitle:
+        f.write(f"{line['text']}\n")
+
+print(f"Subtitle saved to: {filename}.")
